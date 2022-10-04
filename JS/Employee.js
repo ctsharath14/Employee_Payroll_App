9e7547b47a70f1c04088331cd63bc385ee011ddf
ctshara14 <ctsharath14@gmail.com>
2022-10-04 12:29:04 +0530

    Added: Employee name & start date validation on setter methods

--- JS/Employee.js
+++ JS/Employee.js
@@ -1,9 +1,31 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-
+    validName();
+    salaryRange();
+})
+function validName(){
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length < 3 ) {
+            textError.textContent = "Name should have minimum 3 characters!";
+            return;
+        }
+        try {
+            let empData=new EmployeePayrollData();
+            empData.name=name.value;
+            textError.textContent = "";
+        }
+        catch (e) {
+            textError.textContent = e;
+        }
+    });
+}
+/** set event listener on salary range*/
+function salaryRange(){
     const salary = document.querySelector('#salary');
     const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
     salary.addEventListener('input', function () {
         output.textContent = salary.value;
     });
-});
+}
--- JS/NewEmployee.js
+++ JS/NewEmployee.js
@@ -1,51 +1,84 @@
-class EmployeePayroll {
+class EmployeePayrollData {
+    get id() {
+        return this.id
+    }
+    
+    set id(id) {
+        this._id = id;
+    }
 
     get name() {
-        return this._name;
+        return this.name; 
     }
+   
     set name(name) {
-        this._name=name;
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
+        if (nameRegex.test(name))
+            this._name = name;
+        else throw 'Name is Incorrect !';
     }
-
-    get profilePic(){
-        return this._profilePic;
+   
+    get profilePic() {
+         return this.profilePic; 
     }
-    set profilePic(profilePic){
-        this._profilePic=profilePic;
+   
+    set profilePic(profilePic) {
+        this.profilePic = profilePic;
     }
-
-    get gender(){
-        return this._gender;
+   
+    get gender() { 
+        return this._gender; 
     }
-    set gender(gender){
-        this._gender=gender;
+   
+    set gender(gender) {
+        this._gender = gender;
     }
-
-    get department(){
-        return this._department;
+   
+    get department() { 
+        return this._department; 
     }
-    set department(department){
-        this._department=department;
+   
+    set department(department) {
+        this._department = department;
     }
-
-    get salary(){
-        return this._salary;
+   
+    get salary() { 
+        return this._salary; 
     }
-    set salary(salary){
-        this._salary=salary;
+   
+    set salary(salary) {
+        this._salary = salary;
     }
-
-    get note(){
-        return this._note;
+   
+    get note() { 
+        return this._note; 
     }
-    set note(note){
-        this._note=note;
+   
+    set note(note) {
+        this._note = note;
     }
-
-    get startDate(){
-        return this._startDate;
+    
+    get startDate() { 
+        return this._startDate; 
     }
-    set startDate(startDate){
-        this._startDate=startDate;
+    set startDate(startDate) {
+        let currentDate = new Date();
+        if(startDate > currentDate){
+            throw "Start date is a future date";
+        }
+        var diff = Math.abs(currentDate.getTime() - startDate.getTime());
+        if(diff / (1000*60*60*24) > 30){
+            throw "Start date is beyond 30 days";
+        }
+        this._startDate = startDate;
+    }
+    
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric'};
+        const empDate = !this.startDate ? "undefined" :
+            this.startDate.toLocalDateString("en-US", options);
+        return "id=" + this.id + ", name='" + this.name +", gender='" + this.gender +
+                ", profilePic='" + this.profilePic + ", department=" + this.department +
+                ", salary=" + this.salary + ", startDate=" +empDate +", note=" + this.note;
     }
 }
